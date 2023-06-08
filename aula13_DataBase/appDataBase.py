2232645a060388e058b8a2ecb941c1fd7a69cfef
Marcelo Gabriel <marcelogabrielcn@gmail.com>
2023-06-08 19:42:54 -0300

    Add files via upload

--- aula13_DataBase/appDataBase.py
+++ aula13_DataBase/appDataBase.py
@@ -5,14 +5,31 @@ ScreenManager:
     id: screen_manager
     HomeDB:
         id: home
+    Usuarios:
+        id: usuarios
+
 
 <HomeDB>:
     nome: "home"
+    BoxLayout:
+    	orientation: "horizontal"
+    	MDTopAppBar:
+    		title: "App Mobile Database"
+    		valign: "center"
+    		md_bg_color: app.theme_cls.primary_color
+    		elevation: 10
+    		pos_hint: {'center_x': 0.95, 'center_y': 0.95}
+    		left_action_items: [["menu", lambda x: root.menu_esquerda(x, app.theme_cls.accent_color)]]
     MDLabel:
         text: "Nova aplicação para começar usar banco de dados"
         font_size: 18
         color: app.theme_cls.primary_color
         size_hint_x: 0.5
         pos_hint: {'center_x': 0.5, 'center_y': 0.5}   
+
+        
+<Usuarios>:
+    name: "usuarios"
+
 
 '''
--- aula13_DataBase/main.py
+++ aula13_DataBase/main.py
@@ -2,18 +2,45 @@ from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.core.window import Window
+from kivymd.uix.menu import MDDropdownMenu
+from kivy.metrics import dp
+import sqlite3
 from appDataBase import appdb_screen
 
 
 Window.size = (800,600)
 
 class HomeDB(Screen):
-    pass
+    def menu_esquerda(self, button, color):
+        itens_menu = [
+            {
+                'viewclass':'OneLineListItem',
+                'color': [0.1, 0, 0, 1],
+                'text': 'Cadastrar Usuário',
+                'height': dp(40),
+                'on_release': lambda x="Cadastrar Usuário": Usuarios.cadastrar_usuario(self, button)
+            },
+        ]
+        self.menu = MDDropdownMenu(
+                caller=button,
+                items=itens_menu,
+                width_mult=3,
+                background_color=color,
+        )
+        self.menu.open()
+
+        def fechar_menu(self, button):
+            self.menu.caller = button
+            self.menu.dismiss()
+
+class Usuarios(Screen):
+	def cadastrar_usuario(self, button):
+		pass
 
 
 sm = ScreenManager()
 sm.add_widget(HomeDB(name='home'))
-
+sm.add_widget(Usuarios(name='usuarios'))
 
 class AppDataBase(MDApp):
     def __init__(self, **kwargs):
@@ -22,6 +49,20 @@ class AppDataBase(MDApp):
     def build(self):
         self.theme_cls.primary_palette = "Blue"
         self.theme_cls.accent_palette = "Orange"
+
+        conn = sqlite3.connect('datas.db')
+        c = conn.cursor()
+        c.execute("""CREATE TABLE if not exists Usuarios(
+            	nome text,
+            	email text,
+            	senha text,
+            	grupo text,
+            	dt_nascimento date
+        )
+        """)
+
+        conn.commit()
+        conn.close()
 
         return Builder.load_string(appdb_screen)
     
